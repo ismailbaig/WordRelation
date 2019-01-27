31e1efdf7cfe657bf079b2c62e2b5b5b990866a1
ismail1986 <ismailece2007@gmail.com>
2019-01-27 18:12:26 +0530

    initial draft

---
+++ WordsRelation/DefaultPage.aspx
@@ -0,0 +1,190 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Home.Master" AutoEventWireup="true" CodeBehind="DefaultPage.aspx.cs" Inherits="WordsRelation.DefaultPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/datepicker3.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	
+	<!--Custom Font-->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	
+
+
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+
+
+
+    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">New Relation</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Make New Relation</h1>
+			</div>
+		</div><!--/.row-->
+				
+		
+		<div class="row">
+			<div class="col-lg-12">
+			
+				
+				
+				<div class="panel panel-default">
+				
+					<div class="panel-body">
+						<div class="col-md-8">
+							<form role="form">
+								
+			
+								<div class="form-group">
+									<label>File input</label>
+									<%--<input type="file">--%>
+                                    <asp:FileUpload ID="FileUploadContent" runat="server" />
+									<%--<p class="help-block">Example block-level help text here.</p>--%>
+                                    <br />
+                                    <p>
+                                     <asp:Button class="btn btn-primary" ID="btnUploadFile" runat="server"
+                                         Text="Upload" OnClick="btnUploadFile_Click"/>
+                                    </p>
+                                    <p>
+                                        <asp:Label ID="lblMessage" runat="server" Text="Message"></asp:Label>
+                                    </p>
+                                    <p>
+                                     <asp:Button class="btn btn-primary" ID="btnDisplayFile" 
+                                         runat="server" Text="Display" OnClick="btnDisplayFile_Click"/>
+                                    </p>
+								</div>
+								<div class="form-group">
+									<%--<label>Text area</label>
+									<textarea class="form-control" rows="3"></textarea>--%>
+                                    <label>File View</label>
+                                    <div>
+                                     <asp:PlaceHolder ID="iframeDocViewer" runat="server"/>
+                                    </div>
+								</div>
+						
+								</div>
+								<div class="col-md-4">
+							<div class="form-group">
+									<label>Concept 1</label>
+									<%--<input class="form-control" placeholder="Placeholder">--%>
+                                <asp:TextBox ID="txtConcept1" class="form-control" runat="server"></asp:TextBox>
+								</div>
+								<div class="form-group">
+									<label>Concept 2</label>
+									<%--<input type="password" class="form-control">--%>
+                                     <asp:TextBox ID="txtConcept2" class="form-control" runat="server"></asp:TextBox>
+								</div>
+									<div class="form-group">
+										<label>Relation Type</label>
+                                         <asp:TextBox ID="tags1" runat="server" class="form-control" value="" maxlength="20" style="width:50%"></asp:TextBox>
+									
+                                       <%-- <div class="ui-widget">
+  <label for="tags">Tags: </label>
+  <asp:TextBox runat="server" id="tags" ></asp:TextBox>
+</div>--%>
+                                        
+                                        <%--	<select class="form-control">
+											<option>Option 1</option>
+											<option>Option 2</option>
+											<option>Option 3</option>
+											<option>Option 4</option>
+										</select>--%>
+									</div>
+							<%--		<div class="form-group">
+										<label>Multiple Selects</label>
+										<select multiple class="form-control">
+											<option>Option 1</option>
+											<option>Option 2</option>
+											<option>Option 3</option>
+											<option>Option 4</option>
+										</select>
+									</div>--%>
+									<%--<button type="submit" class="btn btn-primary">Submit Button</button>--%>
+									<asp:Button class="btn btn-primary" ID="SaveConceptsRelation" runat="server" Text="Save" OnClick="SaveConceptsRelation_Click"/>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div><!-- /.panel-->
+			</div><!-- /.col-->
+		<%--	<div class="col-sm-12">
+				<p class="back-link">Lumino Theme by <a href="https://www.medialoot.com">Medialoot</a></p>
+			</div>--%>
+		</div><!-- /.row -->
+
+
+
+
+
+    <script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/chart.min.js"></script>
+	<script src="js/chart-data.js"></script>
+	<script src="js/easypiechart.js"></script>
+	<script src="js/easypiechart-data.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="js/custom.js"></script>
+	<script>
+		window.onload = function () {
+	var chart1 = document.getElementById("line-chart").getContext("2d");
+	window.myLine = new Chart(chart1).Line(lineChartData, {
+	responsive: true,
+	scaleLineColor: "rgba(0,0,0,.2)",
+	scaleGridLineColor: "rgba(0,0,0,.05)",
+	scaleFontColor: "#c5c7cc"
+	});
+};
+	</script>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+  } );
+  </script>
+</asp:Content>
---
+++ WordsRelation/DefaultPage.aspx.cs
@@ -0,0 +1,238 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using WordsRelation.Data;
+
+namespace WordsRelation
+{
+    public partial class DefaultPage : System.Web.UI.Page
+    {
+
+        static string DocxConvertedToHtmlDirectory = "DocxConvertedToHtml/";
+        string fileNameFromUser = String.Empty;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SaveConceptsRelation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUploadFile_Click(object sender, EventArgs e)
+        {
+            if (FileUploadContent.HasFile)
+            {
+                try
+                {
+                    fileNameFromUser = string.Empty;
+                    fileNameFromUser = FileUploadContent.FileName;
+
+                    var fiFileName = new FileInfo(fileNameFromUser);
+                    if (Util.IsWordprocessingML(fiFileName.Extension))
+                    {
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            memoryStream.Write(FileUploadContent.FileBytes, 0,
+                                FileUploadContent.FileBytes.Length);
+
+                            using (WordprocessingDocument wDoc =
+                                WordprocessingDocument.Open(memoryStream, true))
+                            {
+                            }
+
+                            lblMessage.Text = "File Uploaded Successfully";
+                            Session["ByteArray"] = FileUploadContent.FileBytes;
+                            Session["FileNameFromUser"] = fileNameFromUser;
+                        }
+                    }
+                    else
+                    {
+                        lblMessage.Text = "Error: Not a WordprocessingML document";
+                        Session["ByteArray"] = null;
+                        Session["FileNameFromUser"] = null;
+
+                    }
+
+                }
+
+                catch (Exception ex)
+                {
+                    lblMessage.Text = "Error: " + ex.Message.ToString();
+                    Session["ByteArray"] = null;
+                    Session["FileNameFromUser"] = null;
+                }
+            }
+            else
+            {
+                lblMessage.Text = "You have not specified a file.";
+                Session["ByteArray"] = null;
+                Session["FileNameFromUser"] = null;
+
+            }
+        }
+
+
+
+        protected void btnDisplayFile_Click(object sender, EventArgs e)
+        {
+            byte[] byteArray = (byte[])(Session["ByteArray"]);
+
+            if (byteArray != null)
+            {
+                try
+                {
+                    DirectoryInfo convertedDocsDirectory =
+                        new DirectoryInfo(Server.MapPath(DocxConvertedToHtmlDirectory));
+
+                    if (!convertedDocsDirectory.Exists)
+                        convertedDocsDirectory.Create();
+
+                    Guid g = Guid.NewGuid();
+                    var htmlFileName = Session["FileNameFromUser"].ToString().Replace(".docx", "").Replace(" ", "$sPaC" +
+                        "" +
+                        "e$").ToString()
+                        + "_" + g.ToString() + ".html";
+                    ConvertToHtml(byteArray
+                        , convertedDocsDirectory, htmlFileName);
+
+                    //Store in Database the file name and path
+                    using (ConceptsRelationDBEntities1 context = new ConceptsRelationDBEntities1())
+                    {
+                        context.MasterFiles.Add(new MasterFile
+                        {
+                            FilePath = convertedDocsDirectory.Name + @"\"+ htmlFileName,
+                            Name = htmlFileName
+                        });
+
+                        context.SaveChanges();
+                    }
+
+                    iframeDocViewer.Controls.Add(new LiteralControl("<iframe width='560' height='315' " +
+                        "src=" + @"DocxConvertedToHtml\" + htmlFileName + "></iframe><br />"));
+
+                }   
+                catch (Exception ex)
+                {
+                    lblMessage.Text = "Error: " + ex.Message.ToString();
+                }
+            }
+            else
+            {
+                lblMessage.Text = "You have not specified a file.";
+            }
+        }
+
+        private static void ConvertToHtml(byte[] byteArray, DirectoryInfo destDirectory,
+            string htmlFileName)
+        {
+            FileInfo fiHtml = new FileInfo(Path.Combine(destDirectory.FullName, htmlFileName));
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                memoryStream.Write(byteArray, 0, byteArray.Length);
+                using (WordprocessingDocument wDoc =
+                        WordprocessingDocument.Open(memoryStream, true))
+                {
+                    var imageDirecotryFullName =
+                        fiHtml.FullName.Substring(0, fiHtml.FullName.Length - fiHtml.Extension.Length) + "_files";
+                    var imageDirectoryRelativeName =
+                        fiHtml.Name.Substring(0, fiHtml.Name.Length - fiHtml.Extension.Length) + "_files";
+                    int imageCounter = 0;
+                    var pageTitle = (string)wDoc
+                                    .CoreFilePropertiesPart
+                                    .GetXDocument()
+                                    .Descendants(DC.title)
+                                    .FirstOrDefault();
+                    HtmlConverterSettings settings = new HtmlConverterSettings()
+                    {
+                        PageTitle = pageTitle,
+                        FabricateCssClasses = true,
+                        CssClassPrefix = "pt-",
+                        RestrictToSupportedLanguages = false,
+                        RestrictToSupportedNumberingFormats = false,
+                        ImageHandler = imageInfo =>
+                        {
+                            DirectoryInfo localDirInfo = new DirectoryInfo(imageDirecotryFullName);
+                            if (!localDirInfo.Exists)
+                                localDirInfo.Create();
+                            ++imageCounter;
+                            string extension = imageInfo.ContentType.Split('/')[1].ToLower();
+                            ImageFormat imageFormat = null;
+                            if (extension == "png")
+                            {
+                                extension = "gif";
+                                imageFormat = ImageFormat.Gif;
+                            }
+                            else if (extension == "gif")
+                                imageFormat = ImageFormat.Gif;
+                            else if (extension == "bmp")
+                                imageFormat = ImageFormat.Bmp;
+                            else if (extension == "jpeg")
+                                imageFormat = ImageFormat.Jpeg;
+                            else if (extension == "tiff")
+                            {
+                                extension = "gif";
+                                imageFormat = ImageFormat.Gif;
+                            }
+                            else if (extension == "x-wmf")
+                            {
+                                extension = "wmf";
+                                imageFormat = ImageFormat.Wmf;
+                            }
+
+                            if (imageFormat == null)
+                                return null;
+
+                            FileInfo imageFileName = new FileInfo(imageDirecotryFullName + "/image" +
+                                imageCounter.ToString() + "." + extension);
+                            try
+                            {
+                                imageInfo.Bitmap.Save(imageFileName.FullName, imageFormat);
+                            }
+                            catch (System.Runtime.InteropServices.ExternalException)
+                            {
+                                return null;
+                            }
+
+                            XElement img = new XElement(Xhtml.img,
+                                new XAttribute(NoNamespace.src, imageDirectoryRelativeName + "/" + imageFileName.Name),
+                                imageInfo.ImgStyleAttribute,
+                                imageInfo.AltText != null ?
+                                    new XAttribute(NoNamespace.alt, imageInfo.AltText) : null);
+
+                            return img;
+
+
+                        }
+                    };
+
+                    // XElement html = HtmlConverter.ConvertToHtml(wDoc, settings);
+                    XElement html = HtmlConverter.ConvertToHtml(wDoc, settings);
+
+
+                    var body = html.Descendants(Xhtml.body).First();
+                    //body.AddFirst(
+                    //    new XElement(Xhtml.p,
+                    //        new XElement(Xhtml.a,
+                    //            new XAttribute("href", "WebForm1.aspx"), "Go back to upload page")));
+
+                    var htmlString = html.ToString(SaveOptions.DisableFormatting);
+
+                    File.WriteAllText(fiHtml.FullName, htmlString, Encoding.UTF8);
+
+                }
+            }
+        }
+    }
+}
